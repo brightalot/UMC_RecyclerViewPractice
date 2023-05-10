97f107bce3e206be529f1eaf74118d4ad186078b
brightalot <gamja9151@hanyang.ac.kr>
2023-05-11 01:43:26 +0900

    메모 작성 과정 구현

--- RecyclerViewMemo/app/src/main/java/com/example/recyclerviewmemo/MainActivity.kt
+++ RecyclerViewMemo/app/src/main/java/com/example/recyclerviewmemo/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.recyclerviewmemo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewmemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var memoActivityLauncher: ActivityResultLauncher<Intent>
+    private lateinit var adapter: MemoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initializeViews()
+
+        val memos: ArrayList<Memo> = arrayListOf()
+        adapter = MemoAdapter(memos)
+
+        binding.rvMemo.adapter = adapter
+
+        memoActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val memoString = data?.getStringExtra("memo") ?: ""
+                val memo = Memo(memoString)
+                memos.add(memo)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        binding.btnAddMemo.setOnClickListener{
+            val intent = Intent(this, MemoActivity::class.java)
+            memoActivityLauncher.launch(intent)
+        }
+    }
+
+    private fun initializeViews() {
+        binding.rvMemo.layoutManager = LinearLayoutManager(this)
     }
 }
--- RecyclerViewMemo/app/src/main/java/com/example/recyclerviewmemo/MemoActivity.kt
+++ RecyclerViewMemo/app/src/main/java/com/example/recyclerviewmemo/MemoActivity.kt
@@ -1,5 +1,7 @@
 package com.example.recyclerviewmemo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recyclerviewmemo.databinding.ActivityMemoBinding
@@ -10,5 +12,14 @@ class MemoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMemoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener {
+            val memo = binding.etAddMemo.text.toString()
+            val intent = Intent()
+            intent.putExtra("memo", memo)
+            setResult(Activity.RESULT_OK, intent)
+
+            finish()
+        }
     }
 }
